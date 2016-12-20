3f4db36c1b0a9fff8951d1a78f2edcd2ed3b3289
Etienne <notetiene@users.noreply.github.com>
2016-12-19 22:42:42 -0500

    Add minified and normal concat tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /*eslint-env node*/
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 var clean = require('gulp-clean');
@@ -9,21 +10,33 @@ var sourcemaps = require('gulp-sourcemaps');
 var build = './dist/';
 var src = './src/js/componentary.js';
 
+var distFiles = {
+    minified: 'componentary.min.js',
+    simple: 'componentary.js'
+};
+
 
 gulp.task('clean', function() {
     return gulp.src(build, {read: false})
         .pipe(clean());
 });
 
-
-gulp.task('js', ['clean'], function() {
+gulp.task('minified_js', ['clean'], function() {
     return gulp.src(src)
         .pipe(sourcemaps.init())
+        .pipe(concat(distFiles.minified))
         .pipe(uglify({preserveComments: 'license'}))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(build));
 });
 
+gulp.task('js', ['clean'], function() {
+    return gulp.src(src)
+        .pipe(sourcemaps.init())
+        .pipe(concat(distFiles.simple))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(build));
+});
 
 gulp.task('watch', function() {
     return watch(src, ['default']);
